26a563228594bd4eb644895758c5a1e1fbf0acc5
Николай <Kolya_popov_86@tut.by>
2022-08-25 20:11:37 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе
+// число кратным первому.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);         // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue);     // Преобразует строку в целое число
+    return result;                         // Возвращает результат 
+}
+
+int num1;
+int num2;
+int ost;
+num1 = Prompt("Введите первое число >");
+num2 = Prompt("Введите второе число >");
+ost = (num1 % num2);
+if (ost == 0){
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine($"Не кратно");
+}
