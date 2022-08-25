8f460b5367b7056a7ad134b11bb738287a33766f
Николай <Kolya_popov_86@tut.by>
2022-08-25 19:49:08 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);         // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue);     // Преобразует строку в целое число
+    return result;                         // Возвращает результат 
+}
+
+int number;
+number = new Random().Next(10, 100);
+System.Console.WriteLine(number);
+int number1 = number % 10;
+int number2 = number / 10;
+if (number1 > number2)
+{
+    System.Console.WriteLine($"Максимальное число {number1}");
+}
+else
+{
+    System.Console.WriteLine($"Максимальное число {number2}");
+}
