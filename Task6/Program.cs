4d0f58d27d6dcb9b47f916ddfa90caa998cc2dac
Николай <Kolya_popov_86@tut.by>
2022-08-30 21:31:21 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход пять чисел и выводит сумму
+// и среднее африфмитическое этих чисел.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);         // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue);     // Преобразует строку в целое число
+    return result;                         // Возвращает результат 
+}
+int[] inputmassive(int Length)
+{
+    int[] massive = new int[Length];
+    for (int i = 0; i < massive.Length; i++)
+    {
+        massive[i] = Prompt($"Введите {i} элемент >");
+    }
+    return massive;
+}
+    int Summ(int[] massive)
+{        
+    int summ = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        summ = summ + massive[i];
+    }
+    return summ;
+}    
+    int [] massive = inputmassive(3);
+    int summ = Summ(massive);
+    double average = summ / ((double)massive.Length);
+    System.Console.WriteLine($"Сумма равна {summ}, Среднее число равно {average}");
